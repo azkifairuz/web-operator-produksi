2916ffef5d13fdd561080bb4bf351a2c7a226202
azkifairuz <azkiajmal@gmail.com>
2023-03-14 20:09:32 +0700

    connect to database

---
+++ src/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $con = mysqli_connect("localhost","root","","form_operator_produksi");
+    if (!$con) {
+        die("gagal connect: ".mysqli_connect_errno());
+    }else {
+        echo "berhasil connect";
+        mysqli_connect_errno();
+    }
+
+
+?>
