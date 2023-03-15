0255ccd563f159702779842f15d9deb070716082
azkifairuz <azkiajmal@gmail.com>
2023-03-16 02:47:34 +0700

    ubah profile page

--- src/Operator/mainPage.php
+++ src/Operator/mainPage.php
@@ -1,10 +1,12 @@
 <?php
-    session_start();
-    $userNip = $_SESSION['NIP'];
-    include("navbar.php");
-    include("../koneksi.php");
-    $getDataOperator = mysqli_query($con,"SELECT * FROM `data_operator_produksi` WHERE NIP ='$userNip' ")?>
-    
+session_start();
+$userNip = $_SESSION['NIP'];
+include("navbar.php");
+include("../koneksi.php");
+$getDataOperator = mysqli_query($con, "SELECT * FROM `data_operator_produksi` WHERE NIP ='$userNip' ");
+$cekUser = mysqli_num_rows($getDataOperator);
+$data = mysqli_fetch_array($getDataOperator);
+  ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -45,65 +47,61 @@
             </ul>
           </div>
           <div class="bg-pink-400 w-fit lg:h-full lg:w-full flex  gap-2 items-center h-5/6">
-            <div class="profil w-full m-10 flex justify-around gap-5  ">
+            <div class="profil w-full m-10 hidden justify-around gap-5  ">
               <div class="w-20 lg:w-52 lg:h-20 h-20 bg-white">
                 <img class="w-20 lg:w-52 lg:h-52 h-20 text-center"
                   src="https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg"
                   alt="profil" />
               </div>
 
-              <div class="bg-white w-1/2">
-                <table class="border-2  w-full text-left mx-auto table-auto">
-                  <tr>
-                    <th class="border p-2 bg-black text-white">NIP</th>
-                    <td class=" p-2">1211000</td>
-                  </tr>
-                  <tr>
-                    <th class="border p-2 bg-black text-white">Nama</th>
-                    <td class="p-2">1211000</td>
-                  </tr>
-                  <tr>
-                    <th class="border p-2 bg-black text-white">Departemen</th>
-                    <td  class="p-2">1211000</td>
-                  </tr>
-                  <tr>
-                    <th class="border p-2 bg-black text-white">Hire_date</th>
-                    <td class="p-2">1211000</td>
-                  </tr>
-                  <tr>
-                    <th class="border p-2 bg-black text-white">Lokasi</th>
-                    <td class="p-2">1211000</td>
-                  </tr>
-                  <tr>
-                    <th class="border p-2 bg-black text-white">NPWP</th>
-                    <td class="p-2">1211000</td>
-                  </tr>
-                  <tr>
-                    <th class="border p-2 bg-black text-white">Title</th>
-                    <td class="p-2">1211000</td>
-                  </tr>
-                  <!-- <tr class=">
-                    <th class="border p-2">NIP</th>
-                    <th class="border p-2">Nama</th>
-                    <th class="border p-2">departemen</th>
-                    <th class="border p-2">Hire_date</th>
-                    <th class="border p-2">Lokasi</th>
-                    <th class="border p-2">NPWP</th>
-                    <th class="border p-2">Title</th>
-                  </tr>
-                  <tr class="bg-white">
-                    <td class="border">tes</td>
-                    <td class="border">tes</td>
-                    <td class="border">tes</td>
-                    <td class="border">tes</td>
-                    <td class="border">tes</td>
-                    <td class="border">tes</td>
-                    <td class="border">tes</td>
-                  </tr> -->
+              <div class="bg-white  w-1/2">
+                <table class="border-2   w-fit text-left  table-auto">
+                  <?php
+                  if ($cekUser == 0) {
+                    ?>
+                    <tr rowspan=5>
+                      <td colspan=5>Belum Memiliki Data</td>
+                    </tr>
+                    <?php
+                  } else {
+                    
+                      ?>
+                      <tr class="border border-black">
+                        <th class="border w-fit p-2 bg-black text-white">NIP</th>
+                        <td class="p-2"><?php echo $data['NIP'] ?></td>
+                      </tr>
+                      <tr class="border border-black">
+                        <th class="border w-fit p-2 bg-black text-white">Nama</th>
+                        <td class="p-2 w-full "><?php echo $data['nama'] ?></td>
+                      </tr>
+                      <tr class="border border-black">
+                        <th class="border w-fit p-2 bg-black text-white">Departemen</th>
+                        <td class="p-2"><?php echo $data['departemen'] ?></td>
+                      </tr>
+                      <tr class="border border-black">
+                        <th class="border w-fit p-2 bg-black text-white">Hire_date</th>
+                        <td class="p-2"><?php echo $data['hire_date'] ?></td>
+                      </tr>
+                      <tr class="border border-black">
+                        <th class="border  p-2 bg-black text-white">Lokasi</th>
+                        <td class="p-2"><?php echo $data['lokasi'] ?></td>
+                      </tr>
+                      <tr class="border border-black">
+                        <th class="border p-2 bg-black text-white">NPWP</th>
+                        <td class="p-2"><?php echo $data['NPWP'] ?></td>
+                      </tr>
+                      <tr class="border border-black">
+                        <th class="border p-2 bg-black text-white">title</th>
+                        <td class="p-2"><?php echo $data['title'] ?></td>
+                      </tr>
+                      <?php
+                    
+                  }
+                  ?>
                 </table>
               </div>
             </div>
-            <div class="ubah-profil hidden  items-center justify-evenly gap-4 w-full">
+            <div class="ubah-profil flex  items-center justify-evenly gap-4 w-full">
               <div class="w-20 h-20 lg:w-52 lg:h-52 bg-white">
                 <img class="w-20 h-20 lg:w-52 lg:h-52 text-center"
                   src="https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg"
@@ -111,34 +109,41 @@
               </div>
               <form action="" method="post" class="form auto flex w-1/2  justify-start flex-col gap-4">
 
-                <div>
-                  <label for="name" class="text-white text-lg">NIP : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
+                <div class="grid grid-cols-2  gap-4">
+                    <div>
+                      <div class="flex flex-col capitalize text-white">
+                        <label for="NIP">NIP</label>
+                        <input class="py-2 text-black px-4 rounded-md" type="text" name="NIP" readonly value="<?php echo $data["NIP"] ?>">
+                      </div>
+                      <div class="flex flex-col capitalize text-white">
+                        <label for="nama">Nama</label>
+                        <input class="py-2 px-4 text-black rounded-md" type="text" name="Nama"  value="<?php echo $data["nama"] ?>">
+                      </div>
+                      <div class="flex flex-col capitalize text-white">
+                        <label for="departemen">Departemen</label>
+                        <input class="py-2 px-4 text-black rounded-md" type="text" name="departemen" readonly value="<?php echo $data["departemen"] ?>">
+                      </div>
+                      <div class="flex flex-col capitalize text-white">
+                        <label for="hire_date">hire_date</label>
+                        <input class="py-2 px-4 text-black rounded-md" type="text" name="hire_date" readonly value="<?php echo $data["hire_date"] ?>">
+                      </div>
+                  </div>
+                  <div>
+                    <div class="flex flex-col capitalize text-white">
+                      <label for="lokasi">lokasi</label>
+                      <input class="py-2 px-4 text-black rounded-md" type="text" name="lokasi" readonly value="<?php echo $data["lokasi"] ?>">
+                    </div>
+                    <div class="flex flex-col capitalize text-white">
+                      <label for="NPWP">NPWP</label>
+                      <input class="py-2 px-4 text-black rounded-md" type="text" name="NPWP" readonly value="<?php echo $data["NPWP"] ?>">
+                    </div>
+                    <div class="flex flex-col capitalize text-white">
+                      <label for="title">title</label>
+                      <input class="py-2 px-4 text-black rounded-md" type="text" name="title" readonly value="<?php echo $data["title"] ?>">
+                    </div>
+                  </div>
                 </div>
-                <div>
-                  <label for="name" class="text-white text-lg">Departemen : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
-                </div>
-                <div>
-                  <label for="name" class="text-white text-lg">Hire_date : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
-                </div>
-                <div>
-                  <label for="name" class="text-white text-lg">Lokasi : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
-                </div>
-                <div>
-                  <label for="name" class="text-white text-lg">NPWP : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
-                </div>
-                <div>
-                  <label for="name" class="text-white text-lg">Title : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
-                </div>
-                <div>
-                  <label for="name" class="text-white text-lg">Nama : </label>
-                  <input class="py-2 px-4 rounded-md" type="text" name="name" placeholder="Masukan Nama">
-                </div>
+                
               </form>
             </div>
           </div>
