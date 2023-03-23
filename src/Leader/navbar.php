1f6120202440e11e097c033af8e6050be3c5433b
azkifairuz <azkiajmal@gmail.com>
2023-03-23 22:11:28 +0700

    feat:navbar

---
+++ src/Leader/navbar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../dist/output.css">
+</head>
+
+<body>
+    <nav class="fixed text-white p-2 text-2xl top-0 left-0 right-0 h-fit z-20 bg-purple-800">
+        <ul class="flex justify-between">
+            <li>
+                Laporan Leader
+            </li>
+            <li class="px-4 py-2   ">
+                <a href="logout.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-8 h-8">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                    </svg>
+                </a>
+
+            </li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
